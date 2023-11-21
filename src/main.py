8a80d87b249ffd99e38289730717172f5d7b52d2
Chuu Myet Thwe <CHUUMYET.22@ichat.sp.edu.sg>
2023-11-21 17:01:17 +0800

    Lab 5 Exercise 2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from threading import Thread
 from hal import hal_keypad as keypad
 from hal import hal_lcd as LCD
+import led_control
 
 #Empty list to store sequence of keypad presses
 password = []
@@ -11,7 +12,15 @@ lcd.lcd_clear()
 #Call back function invoked when any key on keypad is pressed
 def key_pressed(key):
     password.append(key)
-
+    lcd.lcd_clear()
+    lcd.lcd_display_string("LED Control", 1)
+    if key == 1:
+        led_control.lcd_display_string("Blink LED", 2)
+        led.led_control_init()  
+    elif key == 0:
+        lcd.lcd_display_string("Off LED", 2)
+        led_control.led_off()
+        
     print(password)
 
 
@@ -21,7 +30,8 @@ def main():
     lcd.lcd_clear()
 
     # Display something on LCD
-    lcd.lcd_display_string("Lab 5", 1)
+    lcd.lcd_display_string("LED Control", 1)
+    lcd.lcd_display_string("0:Off 1:Blink", 2)
 
     # Initialize the HAL keypad driver
     keypad.init(key_pressed)
@@ -30,7 +40,8 @@ def main():
     keypad_thread = Thread(target=keypad.get_key)
     keypad_thread.start()
 
-
 # Main entry point
 if __name__ == "__main__":
     main()
+
+
